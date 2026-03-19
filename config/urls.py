a2e96709539f25d0066812f9303889cd13f63841
Imane Bassou <imane@example.com>
2026-03-19 15:41:39 +0000

    feat(services): add services API endpoints

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
+    path('api/services/', include('services.urls')),
 ]
 
 if settings.DEBUG:
